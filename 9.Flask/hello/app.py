29575bcec961b7198f7c537c9010301bce9576dd
fang xinsheng <420948614@qq.com>
2024-05-04 11:27:39 +0000

    2024年5月4日 19:27

--- 9.Flask/hello/app.py
+++ 9.Flask/hello/app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    
-    return render_template('index.html')
+    name = request.args.get('name')
+    return render_template('index.html',placeholder=name)
