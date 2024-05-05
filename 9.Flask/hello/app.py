ef05aba4f1f3ae31c97af7f8e983fd3a8e7a800f
fang xinsheng <420948614@qq.com>
2024-05-05 07:45:20 +0000

    2024年5月5日 15:45

--- 9.Flask/hello/app.py
+++ 9.Flask/hello/app.py
@@ -11,4 +11,4 @@ def index():
 @app.route('/greet')
 def greet():
     name = request.args.get('name','world')
-    return render_template('greet.index.html',name=name)
+    return render_template('greet.html',name=name)
