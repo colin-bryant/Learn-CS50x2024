d044067c9d9787c25c54a5bcab9082c48e7fc254
fang xinsheng <420948614@qq.com>
2024-05-05 14:46:48 +0000

    2024年5月5日 22:46

--- 9.Flask/froshims0/app.py
+++ 9.Flask/froshims0/app.py
@@ -20,7 +20,7 @@ def register():
     # Validate submission
     # if not request.form.get("Name") or request.form.get("sport") not in SPORTS:
     #     return render_template("failure.html")
-    if not request.form.get("name"):
+    if not request.form.get("name"):-
         return render_template("failure.html")
     for sport in request.form.getlist("sport"):
         if sport not in SPORTS:
@@ -28,3 +28,4 @@ def register():
     # Confirm registration
     return render_template("success.html")
 
+-------------------------------------------
