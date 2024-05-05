a0ba770173f166a5d6bf29c11b1b1228077701d0
fang xinsheng <420948614@qq.com>
2024-05-05 07:44:38 +0000

    2024年5月5日 15:44

--- 9.Flask/hello/app.py
+++ 9.Flask/hello/app.py
@@ -3,12 +3,12 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     
    # name = request.args.get('name','world') # ' ' default value   request.args['name']
     # return render_template('index.html',name=name)
     return render_template('index.html')
+@app.route('/greet')
 def greet():
     name = request.args.get('name','world')
     return render_template('greet.index.html',name=name)
