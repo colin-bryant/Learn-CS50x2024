70ca130a42a3d2ffef4a818f29969f2e7bb8eb5c
fang xinsheng <420948614@qq.com>
2024-05-05 08:17:18 +0000

    2024年5月5日 16:17

--- 9.Flask/hello/app.py
+++ 9.Flask/hello/app.py
@@ -2,12 +2,16 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     
    # name = request.args.get('name','world') # ' ' default value   request.args['name']
     # return render_template('index.html',name=name)
+    if request.method == 'POST':
+        name = request.form['name']
+        return render_template('index.html',name=name)
     return render_template('index.html')
+    
 @app.route('/greet',methods=['POST'])
 def greet():
     # name = request.args.get('name','world')
