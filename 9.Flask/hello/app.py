658aa1d94825865b26d90dd2e5215e69f140124c
fang xinsheng <420948614@qq.com>
2024-05-05 07:43:45 +0000

    2024年5月5日 15:43

--- 9.Flask/hello/app.py
+++ 9.Flask/hello/app.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 def index():
     
    # name = request.args.get('name','world') # ' ' default value   request.args['name']
-    return render_template('index.html',name=name)
+    # return render_template('index.html',name=name)
+    return render_template('index.html')
+def greet():
+    name = request.args.get('name','world')
+    return render_template('greet.index.html',name=name)
