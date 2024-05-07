52ada00ca47a030b7255264c11cbcbaea24f4999
fang xinsheng <420948614@qq.com>
2024-05-07 09:21:02 +0000

    2024年5月7日 17:21

--- 9.Flask/froshims2/app.py
+++ 9.Flask/froshims2/app.py
@@ -1,10 +1,10 @@
 # Implements a registration form using a select menu, validating sport server-side
-
+from cs50 import SQL
 from flask import Flask,render_template,request,redirect
 
 app=Flask(__name__)
 
-REGISTRANTS = {}
+db=SQL("sqlite://froshims.db")
 
 SPORTS = [
     "Basketball",
@@ -17,25 +17,29 @@ SPORTS = [
 def index():
     return render_template("index.html",sports=SPORTS)
 
+@app.route("/deregister",methods=["POST"])
+def deregister():
+
+    #Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id =?",id)
+    return redirect("/registrants")
+
 @app.route("/register",methods=["POST"] )  
 def register():
     # Validate name
-    name = request.form.get("Name")
-    if not name:
-        return render_template("error.html",message="Missing name")
-    # Validate sport
-    sport =request.form.get("sport")
-    if not sport:
-        return render_template("error.html",message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html",message="Invalid sport")        
-    
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    if not name or sport not in SPORTS:
+        return render_template("/failure.html")
 
     #Remember registrant
-    REGISTRANTS[name] = sport
+    db.execute("INSERT INTO registrants(name,sport) VALUES(?,?)",name,sport)
 
     # Confirm registration
     return redirect("/registrants")
 @app.route('/registrants')
 def registrants():
-    return render_template("registrants.html",registrants = REGISTRANTS)
+    registrants=db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html",registrants = registrants)
