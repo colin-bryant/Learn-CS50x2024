cbac70b05bc84ebbf155324ca0cda589a02f3527
fang xinsheng <420948614@qq.com>
2024-05-07 08:40:40 +0000

    2024年5月7日 16:40

---
+++ 9.Flask/froshims2/app.py
@@ -0,0 +1,41 @@
+# Implements a registration form using a select menu, validating sport server-side
+
+from flask import Flask,render_template,request,redirect
+
+app=Flask(__name__)
+
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee",
+]
+
+@app.route("/")
+
+def index():
+    return render_template("index.html",sports=SPORTS)
+
+@app.route("/register",methods=["POST"] )  
+def register():
+    # Validate name
+    name = request.form.get("Name")
+    if not name:
+        return render_template("error.html",message="Missing name")
+    # Validate sport
+    sport =request.form.get("sport")
+    if not sport:
+        return render_template("error.html",message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html",message="Invalid sport")        
+    
+
+    #Remember registrant
+    REGISTRANTS[name] = sport
+
+    # Confirm registration
+    return redirect("/registrants")
+@app.route('/registrants')
+def registrants():
+    return render_template("registrants.html",registrants = REGISTRANTS)
