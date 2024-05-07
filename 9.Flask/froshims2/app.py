1fe97369fdbbdd129394a5b18ac0dbf9f5f93ffb
fang xinsheng <420948614@qq.com>
2024-05-07 12:40:00 +0000

    2024年5月7日 20:40

--- 9.Flask/froshims2/app.py
+++ 9.Flask/froshims2/app.py
@@ -4,7 +4,7 @@ from flask import Flask,render_template,request,redirect
 
 app=Flask(__name__)
 
-db=SQL("sqlite://froshims.db")
+db=SQL("sqlite:///froshims.db")
 
 SPORTS = [
     "Basketball",
@@ -13,7 +13,6 @@ SPORTS = [
 ]
 
 @app.route("/")
-
 def index():
     return render_template("index.html",sports=SPORTS)
 
@@ -22,24 +21,28 @@ def deregister():
 
     #Forget registrant
     id = request.form.get("id")
+    print(id)
     if id:
         db.execute("DELETE FROM registrants WHERE id =?",id)
     return redirect("/registrants")
 
-@app.route("/register",methods=["POST"] )  
+@app.route("/register",methods=["POST"] )
 def register():
     # Validate name
-    name = request.form.get("name")
+    name = request.form.get("Name")
     sport = request.form.get("sport")
     if not name or sport not in SPORTS:
-        return render_template("/failure.html")
+        print(name,sport)
+        return render_template("failure.html")
 
     #Remember registrant
     db.execute("INSERT INTO registrants(name,sport) VALUES(?,?)",name,sport)
 
     # Confirm registration
     return redirect("/registrants")
+
 @app.route('/registrants')
 def registrants():
     registrants=db.execute("SELECT * FROM registrants")
+    print(registrants)
     return render_template("registrants.html",registrants = registrants)
